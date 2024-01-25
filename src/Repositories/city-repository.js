535609c4b5e1f70496dfb2c50dbfdabce11a7cc9
Priyanshu Chaturvedi <messageforpriyanshu@gmail.com>
2024-01-25 13:27:42 +0530

    creation of service layer for city

--- src/Repositories/city-repository.js
+++ src/Repositories/city-repository.js
@@ -10,6 +10,29 @@ class CityRepository{
         }
     };
 
+    async updateCity(cityId,data){
+        try {
+            const city = await City.update(data,{
+                where : {
+                    id : cityId
+                }
+            });
+
+            return city;
+        } catch (error) {
+            throw {error};
+        }
+    };
+
+    async getCity(cityId) {
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+            throw {error};
+        }
+    };
+
     async deleteCity(cityId) {
         try {
             await City.destroy({
